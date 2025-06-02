c6bd41ea15ac33b441482e1180c5d34a3834abb6
Sebastian-Merlassino <168592380+Sebastian-Merlassino@users.noreply.github.com>
2025-06-02 09:58:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
+from flask import Flask, jsonify, request
 import os
-
-from flask import Flask, request, jsonify
-from app.wsaa import obtener_token
-from app.wsfe import emitir_factura
 
 app = Flask(__name__)
 
 @app.route('/emitir', methods=['POST'])
 def emitir():
-    datos = request.json
-    try:
-        ta = obtener_token()
-        resultado = emitir_factura(datos, ta)
-        return jsonify(resultado)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    return jsonify({"mensaje": "El endpoint /emitir funciona correctamente"}), 200
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
